674ba7b6ce5f4fe7ecedee6de606a24715fb8826
gravedigger7863 <ikeakhe.daniel@web.de>
2025-09-25 19:51:43 +0200

    Fix duplicate deferReply issue

--- commands/music/play.js
+++ commands/music/play.js
@@ -16,9 +16,6 @@ module.exports = {
     const query = interaction.options.getString('query');
     const utils = new CommandUtils();
     
-    // Defer the reply first
-    await interaction.deferReply();
-    
     // Check cooldown
     const cooldown = utils.isOnCooldown(interaction.user.id, 'play');
     if (cooldown) {
